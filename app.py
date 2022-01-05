4a266348c9b762faf5d3dc8d203cad64e03eb05c
Casey Masamitsu <cjmasamitsu@gmail.com>
2022-01-05 13:37:05 -0500

    Redesign, mobile optimization

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     app.route('/')
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 @app.route('/projects/')
